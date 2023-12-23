0dbe72358c4531c87da23b5f740cb2f1a9fee5db
Aldrick Rasquinha <57590768+Hexscion@users.noreply.github.com>
2023-12-23 06:57:05 +0530

    Update Question.jsx

--- src/Question.jsx
+++ src/Question.jsx
@@ -4,12 +4,12 @@ function Question(props) {
     const answerElements = props.question.answers.map((answer, index) => {
         const quizStyles = {
             backgroundColor: answer.selected ? '#D6DBF5' : '#F5F7FB',
-            border: answer.selected ? '1px solid #F5F7FB' : '1px solid #4D5B9E',
+            border: answer.selected ? '1px solid #F5F7FB' : '1px solid #4D5B9E'
         }
         const scoreStyles = {
             backgroundColor: answer.correct ? '#94D7A2' : answer.selected ? '#F8BCBC' : '#F5F7FB',
             border: answer.correct ? '1px solid #94D7A2' : answer.selected ? '1px solid #F8BCBC' : '1px solid #4D5B9E',
-            opacity: answer.correct ? 1 : 0.5
+            opacity: answer.selected ? 1 : 0.5
         }
         const styles = props.checkScore ? scoreStyles : quizStyles
         return (
